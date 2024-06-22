47eda3a1a351f64be8f91bf6d321b441abff4210
Joshua Linehan <jlinehan@student.unimelb.edu.au>
2024-06-22 16:24:09 +1000

    added storage of best scores

--- app/src/app/ScoreHandler.java
+++ app/src/app/ScoreHandler.java
@@ -2,8 +2,11 @@ package app;
 
 import java.io.*;
 
+import app.mines.Minesweeper;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 public class ScoreHandler
 {
@@ -11,6 +14,74 @@ public class ScoreHandler
     private static final String FILENAME = "bestScores.json";
 
     public int getBest(int width, int height, int numMines)
+    {
+        JsonObject bestScoresJson = getJson();
+
+        // query json for best score
+        try
+        {
+            return bestScoresJson.getAsJsonObject(String.valueOf(width)).getAsJsonObject(String.valueOf(height)).get(String.valueOf(numMines)).getAsInt();
+        }
+        catch (NullPointerException e)
+        {
+            return NO_BEST;
+        }
+    }
+
+    public void handleResult(int width, int height, int numMines, int score)
+    {
+        String widthKey = String.valueOf(width);
+        String heightKey = String.valueOf(height);
+        String minesKey = String.valueOf(numMines);
+        int previousBest = getBest(width, height, numMines);
+        if ((previousBest == NO_BEST || score < previousBest) && score != Minesweeper.LOSS)
+        {
+            JsonObject bestScoresJson = getJson();
+            if (bestScoresJson == null)
+            {
+                bestScoresJson = new JsonObject();
+            }
+            JsonElement newBest = new JsonPrimitive(score);
+
+            // create new height json object
+            JsonObject heightObject;
+            try
+            {
+                heightObject = bestScoresJson.getAsJsonObject(widthKey).getAsJsonObject(heightKey);
+            }
+            catch (NullPointerException e)
+            {
+                heightObject = new JsonObject();
+            }
+            if (heightObject == null)
+            {
+                heightObject = new JsonObject();
+            }
+            heightObject.add(minesKey, newBest);
+
+            // create new width json object
+            JsonObject widthObject;
+            try
+            {
+                widthObject = bestScoresJson.getAsJsonObject(widthKey);
+            }
+            catch (NullPointerException e)
+            {
+                widthObject = new JsonObject();
+            }
+            if (widthObject == null)
+            {
+                widthObject = new JsonObject();
+            }
+            widthObject.add(heightKey, heightObject);
+
+            // add new data to json
+            bestScoresJson.add(widthKey, widthObject);
+            writeJson(bestScoresJson);
+        }
+    }
+
+    private JsonObject getJson()
     {
         // ensure best scores file exists
         File scoresFile = new File(FILENAME);
@@ -34,16 +105,20 @@ public class ScoreHandler
             throw new RuntimeException(e);
         }
         Gson gson = new Gson();
-        JsonObject bestScoresJson = gson.fromJson(bufferedReader, JsonObject.class);
+        return gson.fromJson(bufferedReader, JsonObject.class);
+    }
 
-        // query json for best score
+    private void writeJson(JsonObject jsonObject)
+    {
         try
         {
-            return bestScoresJson.getAsJsonObject(String.valueOf(width)).getAsJsonObject(String.valueOf(height)).get(String.valueOf(numMines)).getAsInt();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME));
+            writer.write(jsonObject.toString());
+            writer.close();
         }
-        catch (NullPointerException e)
+        catch (IOException e)
         {
-            return NO_BEST;
+            throw new RuntimeException(e);
         }
     }
 }
